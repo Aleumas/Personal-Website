f00d8d92fd6c841bb459dbfc809b498891fb2a3a
Samuel <sasamoah@uwo.ca>
2022-01-28 21:54:46 -0500

    Worked on blog page

--- portfolio/samuel_asamoah/src/App.js
+++ portfolio/samuel_asamoah/src/App.js
@@ -4,7 +4,7 @@ import Background from './components/Background'
 import AboutMe from './components/hero-content/AboutMe';
 import Project from './components/Projects';
 import { ThemeContext } from './theme-context'
-import { Outlet, useParams } from 'react-router-dom';
+import { Outlet, useParams, useLocation } from 'react-router-dom';
 
 function App() {
 
@@ -33,7 +33,7 @@ function App() {
     );
 
     return (
-      (Object.keys(params).length == 0) ? home : <Outlet/>
+      (useLocation().pathname == '/Portfolio') ? home : <Outlet/>
     );
 }
 
--- portfolio/samuel_asamoah/src/components/navigation/Navigation.jsx
+++ portfolio/samuel_asamoah/src/components/navigation/Navigation.jsx
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ThemeContext } from '../../theme-context';
 
 function Navigation() {
 
     const { theme, updateTheme } = useContext(ThemeContext);
     const toggle = () => updateTheme((theme == 'dark' ? 'light' : 'dark'));
+    let navigate = useNavigate();
 
     return (
         <div className='navigation-right'>
             <a onClick={() => {document.getElementById('projects').scrollIntoView({ block: 'start',  behavior: 'smooth' })}} className={theme + ' roboto-mono navigation-link'}> Projects </a>
-            <a href='' className={theme + ' roboto-mono navigation-link'}> Blog </a>
+            <a onClick={() => { navigate('/Portfolio/Blog', { state: {
+				theme: `${theme}`
+			}})}} className={theme + ' roboto-mono navigation-link'}> Blog </a>
             <button className='toggle' onClick={toggle}><img className='toggle-icon' src={require('../../Public/Theme_toggle_' + theme + '.svg')}/></button>
         </div>
     );
