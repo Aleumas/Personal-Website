cad2ca3baccc6d1242580a9cdf3d97244625ad4a
Samuel <sasamoah@uwo.ca>
2022-02-21 22:24:45 -0500

    Revert "Worked on blog page"
    This reverts commit f00d8d92fd6c841bb459dbfc809b498891fb2a3a.

--- portfolio/samuel_asamoah/src/index.js
+++ portfolio/samuel_asamoah/src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom';
 import './styles/global.css';
 import App from './App';
 import Post from './Post';
-import Blog from './Blog';
 import Require from './Public/Post_Images/require';
 
 import {
   BrowserRouter,
   Routes,
   Route,
+  Outlet
 } from "react-router-dom";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root')
 
 ReactDOM.render(
   <BrowserRouter>
       <Routes>
         <Route path='/Portfolio' element={<App/>}>
-          <Route path='Blog' exact element={<Blog/>}> 
+          <Route path='Blog' exact element={<div><Outlet/></div>}> 
             <Route path=':post' exact element={<Post/>}/>
           </Route>
         </Route>
