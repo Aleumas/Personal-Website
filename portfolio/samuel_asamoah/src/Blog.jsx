c619d6bd96f19a9721d7effaced5883ca4082892
Samuel <sasamoah@uwo.ca>
2022-02-21 22:19:37 -0500

    Revert "Worked on blog page"
    This reverts commit 5d204d097d045103da4387ce5fd297ed21b18c25.

--- portfolio/samuel_asamoah/src/Blog.jsx
+++ portfolio/samuel_asamoah/src/Blog.jsx
@@ -1,35 +1,22 @@
 import Background from './components/Background';
-import NavBar from './components/navigation/NavBar';
-import Project from './components/Project';
-import React, {Fragment} from 'react';
 import {
 	Outlet,
+	useParams,
 	useLocation
 } from "react-router-dom";
 
 function Blog() {
 
+	const params = useParams(); 
 	const state = useLocation().state;
-    	const backgroundColor = (state.theme == 'dark') ? 'black' : 'white';
-    	const projects = require('./Public/Projects.json');
-	const blogPosts = projects.filter(project => project.link_type == 'blog');
-	const posts = (
-		blogPosts.map((project, index) =>
-                	<Fragment  key={index}>
-                    		<Project id={index} theme={state.theme} name={project.name} languages={project.languages} summary={project.summary} link_type={project.link_type} link={project.link} />
-                	</Fragment>   
-	));
+
+	console.log(state.theme);
 
 	const blog = (
-            	<Background type='plain' color={backgroundColor}>
-			<div className={state.theme + ' section-1'}>
-				<NavBar isBlog={true} theme={state.theme}/>
-				<h1 className={'spectral header'}>Blog</h1>
-				{posts}
-			</div>
-		</Background>
-	);
-
+		<div>
+			<h1>Hello, world!</h1>
+		</div>
+	)
 	return (
 		(useLocation().pathname == '/Portfolio/Blog') ? blog : <Outlet/>
 	);
