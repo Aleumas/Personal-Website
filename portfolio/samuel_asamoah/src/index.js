ff9d8e8f7557fd2e65d0b04efca9aa6253f33a37
Samuel <sasamoah@uwo.ca>
2022-02-21 23:19:30 -0500

    .

--- portfolio/samuel_asamoah/src/index.js
+++ portfolio/samuel_asamoah/src/index.js
@@ -17,7 +17,7 @@ const rootElement = document.getElementById('root')
 ReactDOM.render(
   <BrowserRouter>
       <Routes>
-        <Route path='/Portfolio' element={<App/>}>
+        <Route path='/' element={<App/>}>
           <Route path='Blog' exact element={<div><Outlet/></div>}> 
             <Route path=':post' exact element={<Post/>}/>
           </Route>
